7591e9cf5aa26f92ba55c3a813d94e169b9253d6
eggate <Tiefanovic33128370>
2023-01-19 15:08:53 +0200

    docker files

---
+++ php/docker-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+[ "$DEBUG" = "true" ] && set -x
+
+PHP_EXT_DIR=/usr/local/etc/php/conf.d
+
+# Enable PHP extensions
+PHP_EXT_COM_ON=docker-php-ext-enable
+
+[ -d ${PHP_EXT_DIR} ] && rm -f ${PHP_EXT_DIR}/docker-php-ext-*.ini
+
+if [ -x "$(command -v ${PHP_EXT_COM_ON})" ] && [ ! -z "${PHP_EXTENSIONS}" ]; then
+  ${PHP_EXT_COM_ON} ${PHP_EXTENSIONS}
+fi
+
+HOST_NAME="magento2.local"
+HOST_IP=$(php -r "putenv('RES_OPTIONS=retrans:1 retry:1 timeout:1 attempts:1'); echo gethostbyname('$HOST_NAME');")
+if [[ "$HOST_IP" == "$HOST_NAME" ]]; then
+  HOST_IP=$(/sbin/ip route|awk '/default/ { print $3 }')
+  printf "\n%s %s\n" "$HOST_IP" "$HOST_NAME" >> /etc/hosts
+fi
+/var/www/html/bin/magento setup:install --base-url=http://magento2.local --backend-frontname=admin --language=en_US --timezone=Asia/Dubai --currency=AED --db-host=db --db-name=magento --db-user=magento --db-password=magento --use-secure=1 --base-url-secure=https://magento2.local --use-secure-admin=1 --admin-firstname=Ahmed --admin-lastname=Allam --admin-email=demo@admin.com --admin-user=admin --admin-password=Admin@123
+exec "$@"
---
+++ readme.md
@@ -0,0 +1,19 @@
+### Docker image to install Magento 2.4.4 and nginx
+### Not working image
+It's my first trial to use docker 
+
+### Succeeded 
+- install Nginx + php8.1-fpm 
+-  add magento2.local to /etc/hosts
+- add magento2.local config file to nginx
+- install makecert to create self signed cert
+- install composer and copy auth.json to $COMPOSER_HOME
+- download Magento2.4.4 using composer
+- run magento2 installation on magento2.local
+- added redis and opensearch service
+
+### Failed
+- tried many ways to connect db service to web service to install magento but with no luck
+
+
+### I'm still trying to make it work and I will
